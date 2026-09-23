87b3dd0391b7b905abb53d23400504cc6e3918ea
Farhan-dev-dot <farhanhan2478@gmail.com>
2026-09-23 16:05:47 +0700

    done masalah

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // Sinkronkan transaksi Midtrans yang sudah melewati masa kedaluwarsa.
+        $schedule->command('pembelian:sync-expired')->everyFiveMinutes();
     }
 
     /**
